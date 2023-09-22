5b85d8f9c982b71e0dfc4b8de16d97b9be8899d5
danidomi <daniel.domingues@olx.com>
2023-09-22 23:17:03 +0100

    feat: wip

--- src/model/Cat.c
+++ src/model/Cat.c
@@ -1,7 +1,8 @@
 #include "Cat.h"
+#include <stdlib.h>
 
 // Constructor:
-Cat * CatCreate(int age, int something) { 
+Cat * CatCreate(int age, int something) {
     Cat * newCat = malloc(sizeof(Cat));
 
     CatInitialize(newCat, age, something);
@@ -19,6 +20,12 @@ void CatInitialize(Cat * this, int age, int something)
 void CatSetAge(Cat * this, int age) {
     this->age = age;
 }
+
+// "Property" setter:
+int CatGetAge(Cat * this) {
+    return this->age;
+}
+
 
 void CatFree(Cat * this) { 
     // Do any other freeing required here.
